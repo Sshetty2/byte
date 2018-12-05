c9326dba3218374d14bc40f3548268a0de354f1f
Sachit <localhost@mlh.io>
2018-12-05 10:20:14 -0500

    sql query for users_followed and users pk

--- week8/dapp_twitter-jinja/run/src/datastores/SQL Join users_followed & tweets.sql
+++ week8/dapp_twitter-jinja/run/src/datastores/SQL Join users_followed & tweets.sql
@@ -1,9 +1,9 @@
 SELECT tweets.pk, tweets.users_pk, tweets.username, tweets.content, tweets.time
 FROM tweets
 JOIN users_followed
-ON users_followed.users_pk = 2
-JOIN tweets
-ON users_followed.followed_pk =  tweets.users.pk;
+ON users_followed.followed_pk =  tweets.users_pk
+WHERE users_followed.users_pk = 2;
+
 
 
 
